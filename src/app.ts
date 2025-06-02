22ffb177a196dc6cef2a97546842ec8baa3c1f5f
urban-calderon <urbanocalderon@outlook.com>
2025-06-02 14:23:00 -0600

    first commit: backend project configuration

---
+++ src/app.ts
@@ -0,0 +1,18 @@
+import { envs } from './config/envs';
+import { Server } from './presentation/server';
+
+(async()=> {
+    main();
+})();
+
+
+function main() {
+    const server = new Server({
+        port: envs.PORT,
+    });
+    
+    server.start();
+}
+
+    
+    
