ca2ebb9366e2ea76907e426afa1e23aafb0d88c6
urban-calderon <urbanocalderon@outlook.com>
2025-06-02 23:35:31 -0600

    fix: cors configuration for frontend requests

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,14 @@
 import express from 'express';
+import cors from 'cors';
 import bodyParser from 'body-parser';
 import { errorMiddleware } from './middlerwares/error.middleware';
 import encryptionRoutes from './routes/encryption.routes';
 
 export const createApp = () => {
   const app = express();
+
+  //Cors
+  app.use(cors());
 
   // Middlewares
   app.use(bodyParser.json());
